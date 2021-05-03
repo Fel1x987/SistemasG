a62440937c1945102c17353a6d717f4161740c48
Felix <fcm68247@udelasalle.edu.mx>
2021-05-02 22:37:10 -0500

    firebase

---
+++ Firebase/basededatos.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBUSimyNuE-ZufUhbxeRSHMzafFqJaVRFk",
+    authDomain: "productos-c17a5.firebaseapp.com",
+    projectId: "productos-c17a5",
+    storageBucket: "productos-c17a5.appspot.com",
+    messagingSenderId: "966307617483",
+    appId: "1:966307617483:web:3026acebc223a5bb78994e"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
