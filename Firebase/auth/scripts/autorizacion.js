8f8bb33f5ed316843ecf165dbf53d3ec40245d6c
Felix <fcm68247@udelasalle.edu.mx>
2021-05-19 20:34:30 -0500

    paypalme

--- Firebase/auth/scripts/autorizacion.js
+++ Firebase/auth/scripts/autorizacion.js
@@ -1,3 +1,16 @@
+auth.onAuthStateChanged(user =>{
+    console.log(user + " onAuthStateChanged");
+    if(user){    
+        db.collection('platillos').onSnapShot(snapshot =>{
+            obtienePlatillos(snapshot.docs);
+        });
+        configurarMenu(user);
+
+    } else {
+        obtienePlatillos([]);
+        configurarMenu();
+    }
+});
 const formaingresar = document.getElementById("formaingresar");
 formaingresar.addEventListener('submit', (e) =>{
     e.preventDefault();
@@ -40,3 +53,49 @@ salir.addEventListener('click', (e) =>{
         alert('Se ha cerrado sesión');
     });
 });
+
+const formaregistrate = document.getElementById("formaregistrate");
+
+formaregistrate.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const correo = formaregistrate['rcorreo'].value;
+    const contrasena = formaregistrate['rcontrasena'].value;
+
+    auth.createUserWithEmailAndPassword(correo, contrasena).then(cred => {
+        console.log("Se creo el usuario " + correo);   
+        return db.collection('usuario').doc(cred.user.uid).set({
+            nombre : formaregistrate['rnombre'].value,
+            telefono : formaregistrate['rtelefono'].value,
+            direccion : formaregistrate['rdireccion'].value
+        });//A la coleccion de user, con esa id recien creado, se hara un set para que cree el docuemnt
+    }).then(()=>{
+        $('#registermodal').modal('hide');
+        formaregistrate.reset();
+        formaregistrate.querySelector('.error').innerHTML = '';
+    }).catch(err =>{        
+        formaregistrate.querySelector('.error').innerHTML = messageError(err.code);
+    });
+});
+
+entrarGoogle = () =>{
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result){
+        var token = result.credential.accessToken;
+        console.log(token);
+
+        var user = result.user;
+        let html = `
+        <p>Nombre: ${user.displayName}</p>
+        <p>Correp: ${user.email}</p>
+        <img src="${user.displayName}"></img>
+        `;
+
+        datosdelacuenta.innerHTML = html;
+        $('#ingresarmodal').modal('hide');
+
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML = '';
+    }).catch(function(error){
+        console.log("Error " + error)
+    });
+}
---
+++ Firebase/auth/scripts/index.js
@@ -0,0 +1,43 @@
+const listaloggedout = document.querySelectorAll('.loged-out');
+const listaloggedin = document.querySelectorAll('.loged-in');
+const datosdelacuenta = document.querySelector('datosdelacuenta');
+
+const configurarMenu = (user) => {
+    if (user) {
+        db.collection('usuarios').doc(user.uid).get().then(doc => {
+            const html = `<p>Nombre: ${doc.data().nombre}</p>
+            <p>Correo: ${user.email}</p>
+            <p>Telefono: ${doc.data().telefono}</p>
+            <p>Direccion: ${doc.data().direccion}</p>`;
+            datosdelacuenta.innerHTML = html;
+        });
+        listaloggedin.forEach(item => item.style.display = 'block');
+        listaloggedout.forEach(item => item.style.display = 'none');
+    } else {
+        listaloggedin.forEach(item => item.style.display = 'none');
+        listaloggedout.forEach(item => item.style.display = 'block');
+    }
+};
+
+const listadeplatillos = document.getElementById("listadeplatillos");
+const obtienePlatillos = (data) => {
+    if (data.length) {
+        let html = '';
+        data.forEach(doc => {            
+            const platillo = doc.data;
+            const columna = `
+            <div class="col-12 col-md-4">
+                <img src="images/${platillo.imagen}" alt="${platillo.nombre}">
+                <p>${platillo.nombre}</p>
+                <p class="text-danger">$ ${platillo.precio}</p>
+                <a href="https://www.paypal.me/felixcardenas987/${platillo.precio}" target="_blank">
+                    <button class="btn btn-primary"> Pagar AHORA!</button>
+                </a>
+            </div>`;
+            html += columna;
+        });
+        listadeplatillos.innerHTML = html;
+    } else {
+        listadeplatillos.innerHTML= '<p class="text-center">Ingrese para ver los elementos uwu</p>';
+    }
+};
