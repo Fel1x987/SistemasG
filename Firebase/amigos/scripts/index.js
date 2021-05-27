cdb110a915fbedeb40055b9e8c393aef3db1e632
Felix <fcm68247@udelasalle.edu.mx>
2021-05-27 10:24:33 -0500

    d

--- Firebase/amigos/scripts/index.js
+++ Firebase/amigos/scripts/index.js
@@ -38,8 +38,8 @@ const obtieneAmigos = (data) => {
     data.forEach(doc => {
         informacion = new google.maps.InfoWindow;
         var pos = { 
-            lat: doc.data().coordenadas.latitude,
-            lng: doc.data().coordenadas.longitude
+            lat: doc.data().coordenadas.lat,
+            lng: doc.data().coordenadas.lng
         };
         informacion.setPosition(pos);
         informacion.setContent(doc.data().nombre);
