b1089bff7f01f9826909eb722e300659536b6496
Felix <fcm68247@udelasalle.edu.mx>
2021-05-27 15:21:43 -0500

    dd

--- final/scripst/map.js
+++ final/scripst/map.js
@@ -1,31 +1,85 @@
-
 const cargarMapa = () => {
 var informacion =  "<h2>Aquí comienza su aventura!</h2>"        
-            if(navigator.geolocation){
-                navigator.geolocation.getCurrentPosition( position => {
-                    var posicion = {
-                        lat: position.coords.latitude,                            lng: position.coords.longitude
-                        }   
-                        var propiedades = {
-                            center: {
-                                lat: position.coords.latitude, lng: position.coords.longitude
-                            },
-                            zoom: 9
-                        };              
-                        const mapa = document.getElementById("map")
-                        const map = new google.maps.Map(mapa,propiedades)                        
-                        let propiedadesMarcador = {
-                            position: posicion,
-                            map,
-                            title: "Marcardor"
-                        }
-                        const marcador = new google.maps.Marker(propiedadesMarcador);
-                        map.setCenter(posicion)
-                        const infowindow = new google.maps.InfoWindow({
-                            content : informacion
-                        })
-                        marcador.addListener("click", ()=>{                        infowindow.open(map,marcador);
-                    })
-                })                                                             
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( position => {
+        var posicion = {
+            lat: position.coords.latitude,                            
+            lng: position.coords.longitude
+            }   
+        var propiedades = {
+            center: {
+            lat: position.coords.latitude, lng: position.coords.longitude
+            },
+            zoom: 9
+            };              
+            const mapa = document.getElementById("map")
+            const map = new google.maps.Map(mapa,propiedades)                        
+            let propiedadesMarcador = {
+                position: posicion,
+                map,
+                title: "Marcardor"
+                }
+                const marcador = new google.maps.Marker(propiedadesMarcador);
+                map.setCenter(posicion)
+                const infowindow = new google.maps.InfoWindow({
+                    content : informacion
+                })
+                marcador.addListener("click", ()=>{                        infowindow.open(map,marcador);
+            })
+        })                                                             
     }
 }
+
+fetch('locations.json')
+.then(function(response){
+    //console.log(response);
+    response.json().then(function(municipios){
+        //console.log(municipios);
+        var datos = document.getElementById("data");
+            var html = `
+            <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                <th scope="col">Municipio</th>
+                <th scope="col">Estado</th>
+                <th scope="col">Habitantes</th>                            
+                </tr>
+            </thead>
+            <tbody>                        
+            `;
+            var coordlines = [];
+        municipios.forEach(municipio => {
+            coordlines.push(municipio.coordenadas);
+            var circle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.5,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center : municipio.coordenadas,
+            radius : Math.sqrt(municipio.habitantes) * 40
+            });
+            html += `
+            <tr>
+            <th scope="row">${municipio.nombre}</th>
+            <td>${municipio.estado}</td>
+            <td>${municipio.habitantes}</td>                        
+            </tr>
+            `;       
+       });
+       //console.log(coordlines);
+       var trazo = new google.maps.Polyline({
+            path: coordlines,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1,
+            strokeWeight: 1,
+            geodesic:true
+       });
+
+       trazo.setMap(map);
+       html += `</tbody>
+                </table>`;
+        datos.innerHTML = html;
+    });
+});
