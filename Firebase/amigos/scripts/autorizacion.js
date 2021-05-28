13495ba6d92e70603969a5089ce07769eb6e2f94
Felix <fcm68247@udelasalle.edu.mx>
2021-05-28 18:20:49 -0500

    dd

--- Firebase/amigos/scripts/autorizacion.js
+++ Firebase/amigos/scripts/autorizacion.js
@@ -1,8 +1,8 @@
-auth.onAuthStateChanged(user =>{
-    console.log(user);
-    if(user){    
+auth.onAuthStateChanged( user =>{
+    if(user){
+        console.log('Usuario entró');
         if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(position => {
+            navigator.geolocation.getCurrentPosition( position =>{            
                 db.collection('usuarios').doc(user.uid).update({
                     coordenadas : {
                         latitude : position.coords.latitude,
@@ -12,99 +12,98 @@ auth.onAuthStateChanged(user =>{
             });
         }
         db.collection('usuarios').onSnapshot(snapshot =>{
-            obtieneAmigos(snapshot.docs);            
-        });        
-        configurarMenu(user);
-    } else {
+            obtieneAmigos(snapshot.docs);
+            configuraMenu(user);
+        }, err => {
+            console.log(err.message);
+        });
+    }
+    else{
+        console.log('Usuario salió');
         obtieneAmigos([]);
-        configurarMenu();
+        configuraMenu();
     }
 });
-const formaingresar = document.getElementById("formaingresar");
-formaingresar.addEventListener('submit', (e) =>{
+
+const formaregistrate = document.getElementById('formaregistrate');
+formaregistrate.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const correo = formaregistrate['rcorreo'].value;
+    const contrasena = formaregistrate['rcontrasena'].value;
+    auth.createUserWithEmailAndPassword(correo,contrasena).then( cred =>{
+        return db.collection('usuarios').doc(cred.user.uid).set({
+            nombre: formaregistrate['rnombre'].value,
+            telefono: formaregistrate['rtelefono'].value,
+            direccion: formaregistrate['rdireccion'].value
+        });
+    }).then( ()=>{
+        $('#registratemodal').modal('hide');
+        formaregistrate.reset();
+        formaregistrate.querySelector('.error').innerHTML = '';
+    }).catch( err => {
+        formaregistrate.querySelector('.error').innerHTML = mensajeError(err.code);
+    });
+});
+
+const salir = document.getElementById('salir');
+salir.addEventListener('click', (e)=>{
+    e.preventDefault();
+    auth.signOut().then(()=>{
+        alert("El usuario ha salido del sistema");
+    });
+});
+function mensajeError(codigo) {
+    let mensaje = '';
+    switch(codigo) {
+        case 'auth/wrong-password':
+          mensaje = 'Su contraseña no es correcta';
+          break;
+        case 'auth/user-not-found':
+            mensaje = 'El usuario no existe o el correo no esta registrado';
+            break;
+        case 'auth/weak-password':
+            mensaje = 'Contraseña débil debe tener al menos 6 caracteres';
+            break;
+        default:
+            mensaje = 'Ocurrió un error al ingresar con este usuario';
+      }
+    return mensaje;
+  }
+
+const formaingresar =  document.getElementById('formaingresar');
+formaingresar.addEventListener('submit',(e)=>{
     e.preventDefault();
     let correo = formaingresar['correo'].value;
     let contrasena = formaingresar['contrasena'].value;
 
-    auth.signInWithEmailAndPassword(correo, contrasena).then(cred =>{
-        console.log("Entro (y)");
+    auth.signInWithEmailAndPassword(correo,contrasena).then( cred =>{
         $('#ingresarmodal').modal('hide');
-        formaingresar.reset();
-        formaingresar.querySelector('.error').innerHTML='';
-    }).catch(err =>{
-        console.log(err);
-        formaingresar.querySelector('.error').innerHTML = messageError(err.code);
-    });
-});
-
-function messageError(codigo){
-    let message = '';
-    switch(codigo){
-        case 'auth/wrong-password':
-            message = "Contraseña Incorrecta";
-            break;
-        case 'auth/user-not-found':
-            message = "Usuario no encontrado";
-            break;
-        case 'auth/weak-password':
-            message = "Contraeña muy débil :(";
-            break;
-        default:
-            message = codigo;
-    }
-    return message;
-}
-
-const salir = document.getElementById("salir");
-salir.addEventListener('click', (e) =>{
-    e.preventDefault();
-    auth.signOut().then(() => {
-        alert('Se ha cerrado sesión');
-    });
-});
-
-const formaregistrate = document.getElementById("formaregistrar");
-
-formaregistrate.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const correo = formaregistrate['rcorreo'].value;
-    const contrasena = formaregistrate['rcontrasena'].value;
-
-    auth.createUserWithEmailAndPassword(correo, contrasena).then(cred => {
-        console.log("Se creo el usuario " + correo);   
-        return db.collection('usuarios').doc(cred.user.uid).set({
-            nombre : formaregistrate['rnombre'].value,
-            telefono : formaregistrate['rtelefono'].value,
-            direccion : formaregistrate['rdireccion'].value
-        });//A la coleccion de user, con esa id recien creado, se hara un set para que cree el docuemnt
-    }).then(()=>{
-        $('#registrarmodal').modal('hide');
-        formaregistrate.reset();
-        formaregistrate.querySelector('.error').innerHTML = '';
-    }).catch(err =>{        
-        formaregistrate.querySelector('.error').innerHTML = messageError(err.code);
-    });
-});
-
-entrarGoogle = () =>{
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        var token = result.credential.accessToken;
-        console.log(token);
-
-        var user = result.user;
-        let html = `
-        <p style="color: white;">Nombre: ${user.displayName}</p>
-        <p style="color: white;">Correo: ${user.email}</p>
-        <img src="${user.photoURL}"></img>
-        `;
-
-        datosdelacuenta.innerHTML = html;
-        $('#ingresarmodal').modal('hide');
-
         formaingresar.reset();
         formaingresar.querySelector('.error').innerHTML = '';
-    }).catch(function(error){
-        console.log("Error " + error)
+    }).catch( err => {
+
+        formaingresar.querySelector('.error').innerHTML = mensajeError(err.code);
+        console.log(err);
+    });
+ 
+});
+entrarGoogle = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        console.log(token);
+        var user = result.user;
+            console.log(user);
+            const html = `
+                <p>Nombre: ${ user.displayName }</p>
+                <p>Correo: ${ user.email}</p>
+                <img src="${ user.photoURL }" width="50px">
+            `;
+            datosdelacuenta.innerHTML = html;
+            $('#ingresarmodal').modal('hide');
+            formaingresar.reset();
+            formaingresar.querySelector('.error').innerHTML = '';        
+        }).catch(function(error) {
+            console.log(error);
     });
 }
--- Firebase/amigos/scripts/index.js
+++ Firebase/amigos/scripts/index.js
@@ -1,10 +1,10 @@
-const datosdelacuenta = document.querySelector('.datosdelacuenta');
 const listaloggedout = document.querySelectorAll('.logged-out');
 const listaloggedin = document.querySelectorAll('.logged-in');
-
-const configurarMenu = (user) => {
-    if (user) {
-        db.collection('usuarios').doc(user.uid).get().then(doc => {
+const datosdelacuenta = document.querySelector('.datosdelacuenta');
+ 
+const configuraMenu = (user) => {
+    if(user){
+        db.collection('usuarios').doc(user.uid).get().then( doc =>{
             const html = `
                 <p>Nombre: ${ doc.data().nombre }</p>
                 <p>Correo: ${ user.email}</p>
@@ -12,41 +12,30 @@ const configurarMenu = (user) => {
                 <p>Dirección: ${ doc.data().direccion }</p>
                 <p>Coordenadas: ${ doc.data().coordenadas.latitude } , ${ doc.data().coordenadas.longitude }</p>
             `;
-
             datosdelacuenta.innerHTML = html;
-        })        
-        listaloggedin.forEach(item => item.style.display = 'block');
-        listaloggedout.forEach(item => item.style.display = 'none');
-    } else {
-        listaloggedin.forEach(item => item.style.display = 'none');
-        listaloggedout.forEach(item => item.style.display = 'block');
+        });
+        listaloggedin.forEach( item => item.style.display = 'block');
+        listaloggedout.forEach( item => item.style.display = 'none');
+     }
+     else
+     {
+        datosdelacuenta.innerHTML = '';
+        listaloggedin.forEach( item => item.style.display = 'none');
+        listaloggedout.forEach( item => item.style.display = 'block');
+     }
+ }
+ 
+ const obtieneAmigos = (data) =>{
+
+    var propiedades = { 
+        center: { 
+                    lat: 21.152639, lng: -101.711598 
+                }, 
+        zoom: 14 
     }
-};
-
-
-const obtieneAmigos = (data) => {
-    var mapa = document.querySelector("#map");
-    console.log(data);
-    var propiedades = {
-        center: {
-            lat: 21.4787645, lng: -101.2159166
-        },
-        zoom: 14
-    }
-    var map = new google.maps.Map(mapa,propiedades);
-
-
-    data.forEach(doc => {
-        informacion = new google.maps.InfoWindow;
-        let posicion = {    
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        }
-        informacion.setPosition(posicion);
-        informacion.setContent(doc.data().nombre);
-        informacion.open(map);
-    });
-    /*data.forEach( doc => {
+    var mapa =  document.getElementById("map")
+    var map = new google.maps.Map(mapa, propiedades);
+    data.forEach( doc => {        
         informacion = new google.maps.InfoWindow;
         var pos = { 
             lat: doc.data().coordenadas.latitude,
@@ -55,7 +44,5 @@ const obtieneAmigos = (data) => {
         informacion.setPosition(pos);
         informacion.setContent(doc.data().nombre);
         informacion.open(map);
-
-    });*/
-
-};
+    });
+ };
