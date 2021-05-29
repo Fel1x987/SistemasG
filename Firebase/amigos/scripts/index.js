b0e919a3391422116fb2a94ccaaed371c98b4607
Felix <fcm68247@udelasalle.edu.mx>
2021-05-29 14:57:43 -0500

    k

--- Firebase/amigos/scripts/index.js
+++ Firebase/amigos/scripts/index.js
@@ -1,10 +1,10 @@
 const listaloggedout = document.querySelectorAll('.logged-out');
 const listaloggedin = document.querySelectorAll('.logged-in');
 const datosdelacuenta = document.querySelector('.datosdelacuenta');
- 
+
 const configuraMenu = (user) => {
-    if(user){
-        db.collection('usuarios').doc(user.uid).get().then( doc =>{
+    if (user) {
+        db.collection('usuarios').doc(user.uid).get().then(doc => {
             const html = `
                 <p>Nombre: ${ doc.data().nombre }</p>
                 <p>Correo: ${ user.email}</p>
@@ -14,35 +14,39 @@ const configuraMenu = (user) => {
             `;
             datosdelacuenta.innerHTML = html;
         });
-        listaloggedin.forEach( item => item.style.display = 'block');
-        listaloggedout.forEach( item => item.style.display = 'none');
-     }
-     else
-     {
-        datosdelacuenta.innerHTML = '';
-        listaloggedin.forEach( item => item.style.display = 'none');
-        listaloggedout.forEach( item => item.style.display = 'block');
-     }
- }
- 
- const obtieneAmigos = (data) =>{
 
-    var propiedades = { 
-        center: { 
-                    lat: 21.152639, lng: -101.711598 
-                }, 
-        zoom: 14 
+        listaloggedin.forEach(item => item.style.display = 'block');
+        listaloggedout.forEach(item => item.style.display = 'none');
+    } else {
+        datosdelacuenta.innerHTML = '';
+        listaloggedin.forEach(item => item.style.display = 'none');
+        listaloggedout.forEach(item => item.style.display = 'block');
     }
-    var mapa =  document.getElementById("map")
+}
+
+const obtieneAmigos = (data) => {
+
+    var propiedades = {
+        center: {
+            lat: 21.152639,
+            lng: -101.711598
+        },
+        zoom: 14
+    }
+
+    var mapa = document.getElementById("map")
     var map = new google.maps.Map(mapa, propiedades);
-    data.forEach( doc => {        
+
+    data.forEach(doc => {
         informacion = new google.maps.InfoWindow;
-        var pos = { 
+
+        var pos = {
             lat: doc.data().coordenadas.latitude,
             lng: doc.data().coordenadas.longitude
         };
+
         informacion.setPosition(pos);
         informacion.setContent(doc.data().nombre);
         informacion.open(map);
     });
- };
+};
